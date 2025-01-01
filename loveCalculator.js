7a8df48fbd0ebe6880f5521939e50d6642a6c4cd
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-31 21:35:02 -0500

    add love calculator

---
+++ loveCalculator.js
@@ -0,0 +1,16 @@
+
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+// var nameOne= input ("What is your name? ")
+// var nameTwo= input ("What is their name name? ")
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1
+console.log(loveScore);
+if (loveScore === 100){
+    console.log("Your love socrer is " + loveScore + "%. You loveeee each other.");
+} else {
+   console.log("I personally don't know of a divorce lawyer, but I can give you a good marriage counselor's number!");
+}
---
+++ mergeSortedArray.js
@@ -0,0 +1,2 @@
+// #4 median of2 sorted arrays
+// given 2 sorted arrays, nums1 and nums2 of size m and n respectively, return
