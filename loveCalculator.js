f4ef65a9f10d09783f228de93ad486239202b6c4
Jessica Kiroff <jesskiroff@gmail.com>
2024-12-31 21:56:11 -0500

    add new if statements

--- loveCalculator.js
+++ loveCalculator.js
@@ -6,11 +6,25 @@
 
 // var nameOne= input ("What is your name? ")
 // var nameTwo= input ("What is their name name? ")
+
+
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1
 console.log(loveScore);
-if (loveScore === 100){
+// if (loveScore === 100){
+//     console.log("Your love socrer is " + loveScore + "%. You loveeee each other.");
+// } else {
+//    console.log("I personally don't know of a divorce lawyer, but I can give you a good marriage counselor's number!");
+// }
+
+if (loveScore > 70){
     console.log("Your love socrer is " + loveScore + "%. You loveeee each other.");
-} else {
-   console.log("I personally don't know of a divorce lawyer, but I can give you a good marriage counselor's number!");
+} 
+
+if (loveScore > 30 && loveScore <= 70 ){
+    console.log("Your love score is " + loveScore + "%");
+}
+
+if(loveScore<= 30){
+    console.log(" Ya'll really don't like each other do you now? ");
 }
